c137ef3d0bec9c00a8b9df38e09d8a6a6291ed8f
naruyoshi001 <naruyoshi.investment@gmail.com>
2025-07-07 10:36:25 +0900

    add css

--- app.py
+++ app.py
@@ -8,12 +8,18 @@ db = SQLAlchemy(app)
 class Pokemon(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), nullable=False)
-    type = db.Column(db.String(20), nullable=False)
+    type1 = db.Column(db.String(10), nullable=False)
+    type2 = db.Column(db.String(10))
 
 @app.route("/")
 def index():
     pokemons = Pokemon.query.all()
     return render_template("index.html", pokemons=pokemons)
+
+@app.route("/detail/<int:pokemon_id>")
+def detail(pokemon_id):
+    pokemon = Pokemon.query.get_or_404(pokemon_id)
+    return render_template("detail.html", pokemon=pokemon)
 
 if __name__ == "__main__":
     app.run(debug=True)
